05e9acca17e2324ad98cb476c0c33c78f3c298fc
nautatva <nnautatva@gmail.com>
2017-11-25 11:36:03 +0530

    Drawer made better

--- app/src/main/java/com/example/nautatvanavlakha/abcd/HomePage.java
+++ app/src/main/java/com/example/nautatvanavlakha/abcd/HomePage.java
@@ -1,13 +1,81 @@
 package com.example.nautatvanavlakha.abcd;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 
 public class HomePage extends AppCompatActivity {
+
+    DrawerLayout mDrawer;
+    ActionBarDrawerToggle mDrawerToggle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if(getActionBar() != null) {
+            // Display the top-left hamburger button
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        // Make the hamburger button work
+        mDrawer = (DrawerLayout) findViewById(R.id.DL);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawer, toolbar, R.string.app_name, R.string.app_name) {
+            @Override
+            public void onDrawerClosed(View drawerView) {
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+            }
+        };
+
+        mDrawerToggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.white));
+
+        mDrawer.addDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
+
+        // toasts the message when ListView item is clicked
+// ListView mDrawerListView = (ListView) findViewById(R.id.left_drawer);
+//        mDrawerListView.setOnItemClickListener(new ListView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                String drawerstring = ("Menu Item at position " + position + " clicked.");
+//                mDrawer.closeDrawer(GravityCompat.START);
+//                Toast.makeText(getApplicationContext(), drawerstring, Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.DL);
+        if (drawer != null && drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        // Handle your other action bar items...
+        return super.onOptionsItemSelected(item);
     }
 }
